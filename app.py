d0e9929e8d69c18b2d3be834347e0cae08ee4d3b
Kavinda <kavindasamarawickkrama2001@gmail.com>
2024-07-06 01:35:48 +0530

    web application

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,redirect
+from helper import preprocessing , vectorizer , predict
 
 app = Flask(__name__)
 
 data = dict()
-reviews = ['Good Product','Bad Product','I like it']
-positve = 2
-negative = 1
+reviews = []
+positve = 0
+negative = 0
 
 @app.route("/")
 def index():
@@ -14,6 +15,22 @@ def index():
     data['negative'] = negative
 
     return render_template("index.html",data = data)
+
+@app.route("/",methods=["post"])
+def get_post():
+    text=request.form["text"]
+    preprocessed_data = preprocessing(text)
+    vectorized_data = vectorizer(preprocessed_data)
+    prediction = predict(vectorized_data)
+
+    if prediction == "Negative" :
+        global negative
+        negative += 1
+    else:
+        global positve
+        positve += 1
+    reviews.insert(0,text)   
+    return redirect(request.url)     
 
 if __name__ == "__main__":
     app.run()
